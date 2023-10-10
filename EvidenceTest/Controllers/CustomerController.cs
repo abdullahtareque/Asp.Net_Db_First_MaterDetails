f485c6a0bd5e0a24d79c22ba88417a54ac339fe9
TATREQUE <abdullahtarequepi@gmail.com>
2023-10-11 03:18:23 +0600

    Add project files.

---
+++ EvidenceTest/Controllers/CustomerController.cs
@@ -0,0 +1,174 @@
+﻿using EvidenceTest.Models;
+using EvidenceTest.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvidenceTest.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private EvidenceDBEntities db = new EvidenceDBEntities();
+        List<Customer> cst = new List<Customer>();
+        List<Order> ord = new List<Order>();
+
+
+        public ActionResult Index(int? customerID = null)
+        {
+            CustOrderVM co = null;
+            if (customerID != null)
+            {
+                ord = (from d in db.Orders where d.CustomerID == customerID select d).ToList();
+                Customer c = db.Customers.Find(customerID);
+                co = new CustOrderVM { CustomerID = c.CustomerID };
+            }
+
+            var items = db.items.ToList();
+            var itemViewModels = items.Select(item => new CustOrderVM
+            {
+                ItemID = item.ItemID,
+                ItemName = item.ItemName
+            }).ToList();
+            ViewBag.ItemList = new SelectList(itemViewModels, "ItemId", "ItemName");
+
+
+            ViewBag.Records = ord;
+
+            TempData["ord"] = ord;
+
+            return View(co);
+        }
+
+        [HttpPost]
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+        public ActionResult index(CustOrderVM co, string ButtonType)
+        {
+            if (ButtonType == "Add")
+            {
+                AddM(co);
+                return PartialView("_PartialPage1");
+            }
+            else if (ButtonType == "Save")
+                SaveM(co);
+            else
+                DeleteM(co.CustomerID);
+            return Json(new { url = Url.Action("list") });
+        }
+
+        [Route("ExamTest")]
+        public ActionResult List()
+        {
+            TempData["sdt"] = "";
+            List<CustOrderVM> c = new List<CustOrderVM>();
+            var a = db.Customers.ToList();
+            foreach (var m2 in a)
+            {
+                c.Add(new CustOrderVM { CustomerID = m2.CustomerID, CutomerName = m2.CutomerName, BillingAddress = m2.BillingAddress, Imagepath=m2.Imagepath });
+            }
+            return View(c);
+        }
+
+
+        public void AddM(CustOrderVM co)
+        {
+            ord = TempData["ord"] as List<Order>;
+            if (ord == null)
+                ord = new List<Order>();
+            ord.Add(new Order() { OrderID = co.OrderID, OrderNo=co.OrderNo, ItemID=co.ItemID, orderDate = co.orderDate, OrderStatus = co.OrderStatus=true });
+
+            ViewBag.records = ord;
+            TempData["ord"] = ord;
+        }
+
+        public void SaveM(CustOrderVM co)
+        {
+            DeleteM(co.CustomerID);
+            Customer c = new Customer() { CustomerID = co.CustomerID, CutomerName = co.CutomerName, BillingAddress = co.BillingAddress, Imagepath = co.Imagepath };
+            db.Customers.Add(c);
+            db.SaveChanges();
+            ord = TempData["ord"] as List<Order>;
+            foreach (Order d in ord)
+            {
+                Order o = new Order() { OrderID = d.OrderID, OrderNo = co.OrderNo, ItemID = co.ItemID, CustomerID = co.CustomerID, orderDate = d.orderDate, OrderStatus = d.OrderStatus };
+                db.Orders.Add(o);
+                db.SaveChanges();
+            }
+
+            TempData["ord"] = "";
+            Session["ord"] = "";
+        }
+
+        public void DeleteM(int CustomerID)
+        {
+            db.Database.ExecuteSqlCommand($"delete Orders where CustomerID='{CustomerID}'");
+            db.Database.ExecuteSqlCommand($"delete Customers where CustomerId='{CustomerID}'");
+            db.SaveChanges();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var customer = db.Customers.Include(c => c.Orders).FirstOrDefault(c => c.CustomerID == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.Orders.RemoveRange(customer.Orders);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+
+        // New Added
+        public ActionResult Edit(int id)
+        {
+            var customer = db.Customers.Find(id);
+            var orders = db.Orders.Where(o => o.CustomerID == id).ToList();
+
+            var viewModel = new CustOrderVM
+            {
+                Customer = customer,
+                Orders = orders
+
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CustOrderVM viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(viewModel.Customer).State = EntityState.Modified;
+                db.SaveChanges();
+
+
+                foreach (var order in viewModel.Orders)
+                {
+                    db.Entry(order).State = EntityState.Modified;
+                }
+                db.SaveChanges();
+
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("List");
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
+    
---
+++ EvidenceTest/Models/ViewModels/CustOrderVM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EvidenceTest.Models.ViewModels
+{
+    public class CustOrderVM
+    {
+        public int CustomerID { get; set; }
+        
+        [Display(Name = "Customer Name")]
+        [Required(ErrorMessage = "Please enter the customer name.")]
+        public string CutomerName { get; set; }
+
+        [Display(Name = "Billing Address")]
+        [Required(ErrorMessage = "Please enter the billing address.")]
+        public string BillingAddress { get; set; }
+        [Display(Name = "Image")]
+        [Required(ErrorMessage = "Please select an image.")]
+        public string Imagepath { get; set; }
+
+
+        public int ItemID { get; set; }
+        [Display(Name = "Item Name")]
+        public string ItemName { get; set; }
+
+        public int OrderID { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Order Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-mm-dd}", ApplyFormatInEditMode = true)]
+        public DateTime orderDate { get; set; }
+        public int OrderNo { get; set; }
+        
+        [Display(Name = "Order Status")]
+        public Nullable<bool> OrderStatus { get; set; }
+
+        public List<Order> Orders { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
